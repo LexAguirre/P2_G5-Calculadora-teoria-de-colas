f55cad4223743f14bd4c44d403e24dfba70691cd
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2022-11-24 06:05:01 -0600

    Implemented receptor MMm

---
+++ src/MMm/MMm_Receptor.js
@@ -0,0 +1,35 @@
+"use strict";
+
+export default class Receptor {
+  constructor(llegada, servicio, canales) {
+    this._llegada = llegada;
+    this._servicio = servicio;
+    this._canales = canales;
+  }
+
+  getLlegada() {
+    return this._llegada;
+  }
+
+  getServicio() {
+    return this._servicio;
+  }
+
+  getCanales() {
+    return this._canales;
+  }
+
+  static readForm() {
+    let inpLlegada = document.querySelector("#llegada");
+    let inpServicio = document.querySelector("#servicio");
+    let inpCanales = document.querySelector("#canales");
+
+    let llegada = Number(inpLlegada.value);
+    let servicio = Number(inpServicio.value);
+    let canales = Number(inpCanales.value);
+
+    let ecuacion = new Receptor(llegada, servicio, canales);
+
+    return ecuacion;
+  }
+}
